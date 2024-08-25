b9aa7a645825068be337dc5907238d265777e7e8
Dali <126081576+soydali@users.noreply.github.com>
2024-08-25 15:46:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,33 @@ var tikla = document.querySelector("#tikla");
 var video = document.querySelector("video");
 var filtre = document.querySelector(".filtre");
 var portfolio = document.querySelector(".portfolio");
+var current = null;
 
-window.addEventListener("DOMContentLoaded", ()=>{
+function randomVideo() {
+    let next;
+    do {
+        next = Math.floor(Math.random() * 2);
+    } while (next === current);
+    
+    current = next;
+    video.src = current === 0 ? "assets/dalisitebg.mp4" : "assets/tok.mp4";
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    randomVideo();
     main.style.display = "flex";
     loader.style.display = "none";
 });
 
-tikla.addEventListener("click", ()=>{
+tikla.addEventListener("click", () => {
     filtre.style.display = "none";
     tikla.style.display = "none";
     
     video.play();
     portfolio.style.display = "flex";
+});
+
+video.addEventListener("ended", () => {
+    randomVideo();
+    video.play();
 });
